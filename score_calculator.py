142998bee74b7b2ed08b7a596a273f45be9d1296
Andrey Budilovskiy <sabudilovskiy@yandex.ru>
2023-06-21 18:09:05 +0000

    Fix artifacts

--- score_calculator.py
+++ score_calculator.py
@@ -3,26 +3,36 @@ import re
 import json
 import sys
 
-# read from stdin result of tests
-result = sys.stdin.read()
+try:
+    sys.stderr = sys.stdout
+    command = "python -m pytest -q db_migrations/tests"
+    # read result of tests
+    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    output = result.stdout
+    errors = result.stderr
+    print(output, file=sys.stdout)
+    print(errors, file=sys.stdout)
 
-print(result)
+    # Extract the number of successful and failed tests from the output
+    success_tests = re.search(r'(\d+) passed', output)
+    error_tests = re.search(r'(\d+) errors', output) 
+    failed_tests = re.search(r'(\d+) failed', output)
+    success_tests_number = float(success_tests.group(1) if success_tests else 0)
+    failed_tests_number = float(failed_tests.group(1) if failed_tests else 0) + float(error_tests.group(1) if error_tests else 0)
+    total_tests_number = success_tests_number + failed_tests_number
+    percentage =  success_tests_number * 100 / total_tests_number
+    score = int(round(percentage / 10))
 
-# Extract the number of successful and failed tests from the output
-success_tests = re.search(r'(\d+) passed', result)
-error_tests = re.search(r'(\d+) errors', result) 
-failed_tests = re.search(r'(\d+) failed', result)
-success_tests_number = float(success_tests.group(1) if success_tests else 0)
-failed_tests_number = float(failed_tests.group(1) if failed_tests else 0) + float(error_tests.group(1) if error_tests else 0)
-total_tests_number = success_tests_number + failed_tests_number
-percentage =  success_tests_number * 100 / total_tests_number
-score = int(round(percentage / 10))
+    print("Number of successful tests:", success_tests_number)
+    print("Number of total tests:", total_tests_number)
+    print("Percentage of completed tests:", percentage)
+    print("Score:", score)
+    data = {"score": score}
 
-print("Number of successful tests:", success_tests_number)
-print("Number of total tests:", total_tests_number)
-print("Percentage of completed tests:", percentage)
-print("Score:", score)
-data = {"score": score}
+    with open("score.json", "w") as file:
+        json.dump(data, file)
 
-with open("score.json", "w") as file:
-    json.dump(data, file)
+    print ("Score saved")
+
+finally:
+    sys.exit(0)
